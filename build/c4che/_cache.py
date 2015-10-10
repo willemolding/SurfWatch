8dc16476df825b1b2688cc1853cf616cb4f35113
willemolding <willemolding@gmail.com>
2015-10-10 09:25:59 -0700

    fixing up merges

--- build/c4che/_cache.py
+++ build/c4che/_cache.py
@@ -1,6 +1,6 @@
 BINDIR = '/usr/local/bin'
 DEFINES = ['RELEASE']
 LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/willemolding/pebble-dev/PebbleSDK-3.6-dp2/Pebble/common'
+PEBBLE_SDK_COMMON = '/Users/willemolding/pebble-dev/PebbleSDK-3.6-dp8/Pebble/common'
 PREFIX = '/usr/local'
 TARGET_PLATFORMS = [u'chalk', u'basalt']
