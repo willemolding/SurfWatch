4619ad2159d5ab0d7973af406e8e0c94c3a01945
willemolding <willemolding@gmail.com>
2015-10-10 09:29:27 -0700

    stops tracking build dir

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/willemolding/pebble-dev/PebbleSDK-3.6-dp8/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'chalk', u'basalt']
